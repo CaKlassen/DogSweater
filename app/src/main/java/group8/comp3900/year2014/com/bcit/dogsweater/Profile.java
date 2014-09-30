616d669c2839f26c8cbc879df725c8e90e18885d
Rhea <rhealauzon@gmail.com>
2014-09-30 14:17:03 -0700

    UI begins..

--- app/src/main/java/group8/comp3900/year2014/com/bcit/dogsweater/Profile.java
+++ app/src/main/java/group8/comp3900/year2014/com/bcit/dogsweater/Profile.java
@@ -1,26 +1,24 @@
 package group8.comp3900.year2014.com.bcit.dogsweater;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class Profile extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.profile, menu);
         return true;
     }
 
@@ -30,21 +28,9 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
-    public void NewProject(View v)
-    {
-        Intent in = new Intent(this, Profile.class);
-        startActivity(in);
-        return;
-
-    }
-
 }
